aa757ab54b2cb4140c5f09a3207540a31d60f56f
mamania0217 <aryanmamania17@gmail.com>
2024-02-29 13:35:39 +0530

    completed sign up page UI

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,11 +1,68 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import { Button, Label, TextInput } from "flowbite-react";
 
 const SignUp = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        <div className=" flex-1">
+          <Link
+            to="/"
+            className="font-bold dark:text-white text-4xl"
+          >
+            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+              Aryan's
+            </span>
+            Blog
+          </Link>
+          <p className="text-sm mt-5">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit atque illum tenetur in tempora? Praesentium accusamus eum nobis velit eligendi officia modi officiis impedit illum rem tempore, nisi aut ducimus.
+          </p>
+        </div>
 
-export default SignUp
+        <div className="flex-1">
+          <form className="flex flex-col gap-4">
+            <div>
+                <Label value= "Your UserName" />
+                <TextInput
+                type="text"
+                placeholder="UserName"
+                id="username"
+                />
+            </div>
+
+            <div>
+                <Label value= "Your Email" />
+                <TextInput
+                type="text"
+                placeholder="Email"
+                id="username"
+                />
+            </div>
+
+            <div>
+                <Label value= "Your Password" />
+                <TextInput
+                type="text"
+                placeholder="Password"
+                id="username"
+                />
+            </div>
+
+            <Button gradientDuoTone='purpleToPink' type='submit'>
+              Sign Up
+            </Button>
+          </form>
+          <div className="flex gap-2 text-sm mt-5">
+            <span className="font-semibold">Have An Account ?</span>
+            <Link to = '/sign-in' className="text-blue-500">
+              Sign In
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
