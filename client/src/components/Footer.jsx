3e8239e9497dd0c7453163276a4daf411f3be253
mamania0217 <aryanmamania17@gmail.com>
2024-03-02 01:22:13 +0530

    small changes in footer section

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -11,7 +11,7 @@ import {
 
 const FooterComp = () => {
   return (
-    <Footer className="border border-t-8 border-teal-500">
+    <Footer container className="border border-t-8 border-teal-500">
       <div className="w-full max-w-7xl mx-auto">
         <div className="grid w-full justify-between sm:flex md:grid-cols-1 ">
           <div className="mt-5">
@@ -26,7 +26,7 @@ const FooterComp = () => {
             </Link>
           </div>
 
-          <div className="grid grid-cols-2 gap-8 sm:mt-4 sm:grid-cols-3 sm:gap-6">
+          <div className="grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6">
             <div>
               <Footer.Title title="About" />
               <Footer.LinkGroup col>
@@ -61,20 +61,22 @@ const FooterComp = () => {
                 </Footer.Link>
 
                 <Footer.Link href="" target="_blank" rel="noopenenr noreferrer">
-                  Legal Condition
+                  Terms &amp; Condition
                 </Footer.Link>
               </Footer.LinkGroup>
             </div>
           </div>
         </div>
         <Footer.Divider />
-        <div className="w-full sm:flex sm:items-center sm:justify-between mb-3">
+
+        <div className="w-full sm:flex sm:items-center sm:justify-between">
           <Footer.Copyright
             href="#"
             by="Aryan Mamania"
             year={new Date().getFullYear()}
           />
-          <div className="flex gap-6 sm:mt-0 mt-4 justify-center">
+
+          <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
             <Footer.Icon href="#" icon={BsFacebook} />
             <Footer.Icon href="#" icon={BsInstagram} />
             <Footer.Icon href="#" icon={BsTwitter} />
