0686e42cae6452f77f758617c803e021b017764d
mamania0217 <aryanmamania17@gmail.com>
2024-03-08 11:28:47 +0530

    updating dash profile

--- client/src/components/DashSideBar.jsx
+++ client/src/components/DashSideBar.jsx
@@ -1,40 +1,42 @@
-import React from 'react'
-import { useEffect, useState } from "react"
-import { useLocation, Link } from "react-router-dom"
-import {Sidebar} from 'flowbite-react'
-import { HiUser , HiArrowSmRight } from "react-icons/hi"
+import React from "react";
+import { useEffect, useState } from "react";
+import { useLocation, Link } from "react-router-dom";
+import { Sidebar } from "flowbite-react";
+import { HiUser, HiArrowSmRight } from "react-icons/hi";
 
 const DashSideBar = () => {
-
-
-    const location = useLocation()
-    const [tab, setTab] = useState('')
-    useEffect(()=>{
-      const urlParams = new URLSearchParams(location.search)
-      const tabFromUrl = urlParams.get('tab')
-      if(tabFromUrl){
-        setTab(tabFromUrl);
-      }
-    }, [location.search])
-
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
 
   return (
-    <Sidebar className='w-full md:w-56'>
-    <Sidebar.Items>
+    <Sidebar className="w-full md:w-56">
+      <Sidebar.Items>
         <Sidebar.ItemGroup>
-            <Link to = '/dashboard?tab=profile'>
-            <Sidebar.Item active={tab === 'profile'} icon={ HiUser } label={"User"} labelColor="dark" as='div'>
-                Profile
+          <Link to="/dashboard?tab=profile">
+            <Sidebar.Item
+              active={tab === "profile"}
+              icon={HiUser}
+              label={"User"}
+              labelColor="dark"
+              as="div"
+            >
+              Profile
             </Sidebar.Item>
-            </Link>
-            <Sidebar.Item active icon={ HiArrowSmRight } className="cursor-pointer">
-                Sign Out    
-            </Sidebar.Item>
-
+          </Link>
+          <Sidebar.Item active icon={HiArrowSmRight} className="cursor-pointer">
+            Sign Out
+          </Sidebar.Item>
         </Sidebar.ItemGroup>
-    </Sidebar.Items>
-</Sidebar>
-  )
-}
+      </Sidebar.Items>
+    </Sidebar>
+  );
+};
 
-export default DashSideBar
+export default DashSideBar;
