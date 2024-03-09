7e473a9bb28e50ec0ee0c18b114329006e23ba05
mamania0217 <aryanmamania17@gmail.com>
2024-03-09 17:02:09 +0530

    added image functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,56 +1,77 @@
 import { TextInput, Button } from "flowbite-react";
 import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
-import {getStorage, uploadBytesResumable} from "firebase/storage";
-import { app } from "../firebase"
+import {
+  getDownloadURL,
+  getStorage,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
 
 const DashProfile = () => {
   const { currentUser } = useSelector((state) => state.user);
-  const [imageFile , setImageFile] = useState(null);
-  const [imageFileUploadingProgress , setImageUploadingProgress] = useState(null);
-  const [imageFileUploadError , setImageUploadError] = useState(null)
-  const filePickRef = useRef
+  const [imageFile, setImageFile] = useState(null);
+  const [imageFileUrl, setImageFileUrl] = useState(null);
+  const [imageFileUploadingProgress, setImageUploadingProgress] =
+    useState(null);
+  const [imageFileUploadError, setImageUploadError] = useState(null);
+  const filePickRef = useRef();
 
-  const handleImageChange = (e) =>{
-    const file = e.target.files[0]
-    if(file){
-       setImageFile(file)
-       setImageFileUrl(URL.createObjectURL(file))
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImageFile(file);
+      setImageFileUrl(URL.createObjectURL(file));
     }
-   
   };
- useEffect(()=>{
-  if(imageFile){
-    uploadImage()
-  }
- }, [imageFile])
+  useEffect(() => {
+    if (imageFile) {
+      uploadImage();
+    }
+  }, [imageFile]);
 
- const uploadImage = async ()=>{
-  console.log("uploading inmage")
- }
+  const uploadImage = async () => {
+    console.log("uploading inmage");
+  };
 
- const storage = getStorage(app);
- const fileName = new Date().getTime() + imageFile.name;
- const storageRef = ref(storage, fileName)
- const uploadTask = uploadBytesResumable(storageRef, imageFile);
- uploadTask.on(
-  'state_changed',
-  (snapshot) =>{
-    const progress = 
-    (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-    imageFileUploadingProgress(progress.toFixed(0));
-  }, 
-  (error)
- )
+  const storage = getStorage(app);
+  const fileName = new Date().getTime() + imageFile.name;
+  const storageRef = ref(storage, fileName);
+  const uploadTask = uploadBytesResumable(storageRef, imageFile);
+  uploadTask.on(
+    "state_changed",
+    (snapshot) => {
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      imageFileUploadingProgress(progress.toFixed(0));
+    },
+    (error) => {
+      setImageUploadError(
+        "Could not Upload image (File must be less than 2 mb)"
+      );
+    },
+    () => {
+      getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl) => {
+        setImageFileUrl(downloadUrl);
+      });
+    }
+  );
 
   return (
     <div className="max-w-lg mx:auto p-3 w-full">
       <h1 className="my-7 text-center font-semibold text-3xl">profile</h1>
       <form className="flex flex-col gap-4">
-        <input type="file" accept="image/*" onChange={handleImageChange} ref = {filePickRef} />
-        <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full" onClick={()=>filePickRef.currrent.click()} >
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleImageChange}
+          ref={filePickRef}
+        />
+        <div
+          className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full"
+          onClick={() => filePickRef.currrent.click()}
+        >
           <img
-            src={ imageFileUrl || currentUser.profilePicture}
+            src={imageFileUrl || currentUser.profilePicture}
             alt="user"
             className="rounded-full w-full h-full object-cover border-8 border-[lightgray]"
           />
