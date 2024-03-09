85c9a70800c8777877703d39422d4c42595c43a1
mamania0217 <aryanmamania17@gmail.com>
2024-03-09 16:51:04 +0530

    working on DashProfile section

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,11 +1,14 @@
 import { TextInput, Button } from "flowbite-react";
 import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
+import {getStorage, uploadBytesResumable} from "firebase/storage";
+import { app } from "../firebase"
 
 const DashProfile = () => {
   const { currentUser } = useSelector((state) => state.user);
   const [imageFile , setImageFile] = useState(null);
-  const [imageFileUrl , setImageFileUrl] = useState(null);
+  const [imageFileUploadingProgress , setImageUploadingProgress] = useState(null);
+  const [imageFileUploadError , setImageUploadError] = useState(null)
   const filePickRef = useRef
 
   const handleImageChange = (e) =>{
@@ -25,6 +28,20 @@ const DashProfile = () => {
  const uploadImage = async ()=>{
   console.log("uploading inmage")
  }
+
+ const storage = getStorage(app);
+ const fileName = new Date().getTime() + imageFile.name;
+ const storageRef = ref(storage, fileName)
+ const uploadTask = uploadBytesResumable(storageRef, imageFile);
+ uploadTask.on(
+  'state_changed',
+  (snapshot) =>{
+    const progress = 
+    (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+    imageFileUploadingProgress(progress.toFixed(0));
+  }, 
+  (error)
+ )
 
   return (
     <div className="max-w-lg mx:auto p-3 w-full">
