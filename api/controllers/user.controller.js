e49e78e151cf7341e819576f1b3afe6eeabcbfaa
mamania0217 <aryanmamania17@gmail.com>
2024-03-11 00:08:22 +0530

    working on backend

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,7 +1,16 @@
+import bcryptjs from 'bcryptjs'
+import { errorHandler } from "../utils/error.js";
+
 export const test = (req, res) =>{
     res.json({message: 'API is working'});
 };
 
 export const updateUser = async(req , res , next)=>{
-    console.log(req.user);
+  if (req.user.id !== req.params.userId){
+  return next (errorHandler(403, 'You are not allowed to update this user'))
+  }
+  if(req.body.password){
+    if(req.body.password.length(400, 'Password must be atleast 6 characters'));
+  }
+  req.body.password = bcryptjs.hashSync(req.body.password, 10)
 }
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,8 @@
 import express from "express";
- const router = express.Router();
  import { test, updateUser } from '../controllers/user.controller.js'
 import { verifyToken } from "../utils/verifyUser.js";
-
+const router = express.Router();
  router.get('/test', test);
- router.put('/update:userId', verifyToken, updateUser)
+ router.put('/update:userId', verifyToken, updateUser);
 
  export default router;
