54df8fb7d750c12dd343216f49b70ddc55e45982
mamania0217 <aryanmamania17@gmail.com>
2024-03-15 11:11:01 +0530

    working on frontend

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -129,6 +129,23 @@ if(!res.ok){
   dispatch(deleteUserFailure(error.mesage))
 }
 }
+
+
+const handleSignOut = async()=>{
+  try{
+const res = await fetch('/api/user/signout', {
+  method: 'POST'
+})
+const data = await res.json();
+if(!res.ok){
+  console.log(data.message);
+}else{
+  
+}
+  }catch(error){
+
+  }
+}
   return (
     <div className="max-w-lg mx:auto p-3 w-full">
       <h1 className="my-7 text-center font-semibold text-3xl">profile</h1>
@@ -195,7 +212,7 @@ if(!res.ok){
       </form>
       <div className="text-red-500 flex justify-between mt-5">
         <span onClick={()=>setShowModal(true)} className="cursor-pointer">Delete Account</span>
-        <span className="cursor-pointer">Sign Out</span>
+        <span onClick={handleSignOut} className="cursor-pointer">Sign Out</span>
       </div>
 
       {updateUserSuccess && (
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -45,6 +45,11 @@ const userSlice = createSlice({
       state.loading = false;
       state.error = action.payload;
     },
+    signoutSuccess: (state)=>{
+      state.currentUser= null;
+      state.error = null;
+      state.loading= false
+    }
   },
 });
 
